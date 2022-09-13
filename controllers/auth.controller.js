622bed87eae78a8c39602b20f5037eb69afbdf81
unknown <Arles Maldonado>
2022-09-13 10:10:49 -0600

    Google Signin implementation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
-const {generarJWT} = require('../helpers/generarJWT')
+const {generarJWT} = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
 
@@ -48,11 +49,61 @@ const login = async (req, res = response) => {
             msg:"Algo salio mal."
         })
     }
+}
 
-    
+const googleSignIn = async ( req, res = response ) => {
+
+    const { id_token } =  req.body;
+
+    try {
+
+        const {name: nombre, picture: img, email:correo} = await googleVerify( id_token );
+
+        // console.log(correo);
+        // return;
+         
+        let usuario = await Usuario.findOne({ correo });  
+
+        if ( usuario == null ) {
+
+            const data = {
+                nombre,
+                correo,
+                password:'Google',
+                img,
+                rol:'USER_ROLE',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if( !usuario.estado ){
+            return res.status(401).json({
+                msg:'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+
+        res.status(400).json({
+            ok:false,
+            msg:error
+        })
+    }
+
 
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth.controller');
+const { login, googleSignIn } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { esRolValido, existeUsuarioporID } = require('../helpers/db-validator');
 const router = Router();
@@ -11,5 +11,10 @@ router.post('/login', [
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ] ,login);
+
+router.post('/google', [
+    check('id_token', "El id_token es necesario").not().isEmpty(),
+    validarCampos
+] ,googleSignIn);
 
 module.exports = router;
