0323e27d4630fb77134f2f861da31a58adba2915
unknown <Arles Maldonado>
2022-09-15 17:15:31 -0600

    Funcion actualizar imagenes local y en cloudinary 2

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -101,7 +101,10 @@ const actualizarImagenCloudinary = async (req, res = response) => {
 
     //Proceso de limpieza en caso de ya exista una imagen
     if ( modelo.img ) {
-        
+        const nombreArr =  modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length - 1];
+        const [ public_id ] = nombre.split('.');
+        cloudinary.uploader.destroy( public_id );
     }
 
 
